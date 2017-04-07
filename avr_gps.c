7706acf68d4c21826cba661e9f8189cbc8a91d31
Arief Ismail <arief.ismailrifqi@gmail.com>
2017-04-07 10:22:38 +0700

    Update avr_gps.c

--- avr_gps.c
+++ avr_gps.c
@@ -1,3 +1,10 @@
+/*****************************************************
+Author                  : Arief Ismail
+Chip type               : ATmega32
+AVR Core Clock frequency: 16.000000 MHz
+Compiler                : CodeVisionAVR 2.05.3
+*****************************************************/
+
 #include <mega32a.h>
 #include <alcd.h>
 #include <stdio.h>
@@ -15,10 +22,12 @@ unsigned char status;
 unsigned char buf[16];
 int bujur;
 
+// Data dari GPS kita ambil dalam kalimat NMEA GPGLL
 interrupt [USART_RXC] void usart_rx_isr(void){
 unsigned char data;
 data=UDR;
     switch(slot){
+    //data $GPGLL
     case 0:if(data=='$')slot=1;break;
     case 1:if(data=='G')slot=2;else if(data=='$')slot=1;break;
     case 2:if(data=='P')slot=3;else if(data=='$')slot=1;break;
@@ -26,25 +35,36 @@ data=UDR;
     case 4:if(data=='L')slot=5;else if(data=='$')slot=1;break;
     case 5:if(data=='L')slot=6;else if(data=='$')slot=1;break;
     case 6:if(data==',')slot=7;else if(data=='$')slot=1;break;
+    
+    //data latitude
     case 7:
     if     (data=='$') slot=1; 
     else if(data==','){z=0;slot=8;}
     else              {latitude[z]=data;z++;}
     break;
+            
+    //indikator N/S
     case 8:if(data=='$')slot=1;else{latitude_indicator=data;slot=9;}break;
     case 9:if(data==',')slot=10;else if(data=='$')slot=1;break;
+    
+    //data longitude    
     case 10:
     if     (data=='$') slot=1; 
     else if(data==','){z=0;slot=11;}
     else              {longitude[z]=data;z++;}
     break;
+    //indicator E/W
     case 11:if(data=='$')slot=1;else{longitude_indicator=data;slot=12;}break;
     case 12:if(data==',')slot=13;else if(data=='$')slot=1;break;
+    
+    //data jam satelit UTC
     case 13:
     if     (data=='$') slot=1; 
     else if(data==','){z=0;slot=14;}
     else              {waktu[z]=data;z++;}
     break;
+    
+    //Indikator data sudah selesai diterima
     case 14:if(data=='$')slot=1;else{status=data;slot=0;data_update=1;}break;
     }
 }
